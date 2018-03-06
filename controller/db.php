7a98781d2f6bc005b226a498478c8b3a6d6bdc06
1194331476 <1194331476@qq.com>
2018-03-06 13:32:11 +0800

    链接数据库，新增用户

---
+++ controller/db.php
@@ -0,0 +1,17 @@
+<?php
+$mysql_conf = array(
+    'host'    => '127.0.0.1:3306', 
+    'db'      => 'policepeople', 
+    'db_user' => 'root', 
+    'db_pwd'  => 'root', 
+);
+ 
+// 创建连接
+$conn = new mysqli($mysql_conf['host'], $mysql_conf['db_user'], $mysql_conf['db_pwd'],$mysql_conf['db']);
+
+// 检测连接
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+} 
+echo "连接成功";
+?>
--- controller/user.php
+++ controller/user.php
@@ -1,6 +1,19 @@
 <?php
+require ('db.php');
+
 $method = $_GET['method'];
+
+
 if($method == "addUser"){
-    
-    echo "新增方法";
+   $data = $_POST['data'];
+   $sql = "INSERT INTO user (username, truename, sex)
+            VALUES ('"+$data['username']+"', '"+$data['truename']+"', '"+$data['sex']+"')";
+   echo $conn->query($sql);
+   /* if ($conn->query($sql) === TRUE) {
+       echo "success";
+   } else {
+       echo "error";
+   } */
+   /* $conn->close();
+   echo json_encode($data); */
 }
