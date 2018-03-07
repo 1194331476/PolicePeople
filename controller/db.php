c059cca9b4260e1c6ff364b056a551ea6d165402
1194331476 <1194331476@qq.com>
2018-03-07 13:12:11 +0800

    分页

--- controller/db.php
+++ controller/db.php
@@ -10,8 +10,8 @@ $mysql_conf = array(
 $conn = new mysqli($mysql_conf['host'], $mysql_conf['db_user'], $mysql_conf['db_pwd'],$mysql_conf['db']);
 
 // 检测连接
-if ($conn->connect_error) {
+/* if ($conn->connect_error) {
     die("连接失败: " . $conn->connect_error);
 } 
-echo "连接成功";
+echo "连接成功"; */
 ?>
--- controller/user.php
+++ controller/user.php
@@ -1,19 +1,31 @@
 <?php
 require ('db.php');
 
-$method = $_GET['method'];
+$method = "getUserListPage";
 
 
 if($method == "addUser"){
+   //新增用户方法-------------------------------------------------------------------------------------------
    $data = $_POST['data'];
    $sql = "INSERT INTO user (username, truename, sex)
-            VALUES ('"+$data['username']+"', '"+$data['truename']+"', '"+$data['sex']+"')";
-   echo $conn->query($sql);
-   /* if ($conn->query($sql) === TRUE) {
+            VALUES ('".$data['username']."', '".$data['truename']."', '".$data['sex']."')";
+   if ($conn->query($sql) == 1) {
        echo "success";
    } else {
        echo "error";
-   } */
-   /* $conn->close();
-   echo json_encode($data); */
+   } 
+}else if($method == "getUserListPage"){
+    //返回用户分页数据----------------------------------------------------------------------------------------
+    $sql="select * from user";
+    //执行查询并获取查询结果
+    $result = $conn->sql($sql);
+    //输出受影响数据行数
+    $num=$conn->getResultNum($sql);
+    echo "影响的行数：".$num;
+    //读取并输出记录
+    while ($row = mysqli_fetch_assoc($result)){
+        echo "{$row['name']} ";
+        echo "{$row['password']}";
+    }
 }
+$conn->close();
